81ccb6d3a70bb1abf2d53801f3875e3c48b513c6
KowshikBh <kowshik.pawan07@gmail.com>
2024-09-28 21:18:09 -0500

    Rand intger

--- data.py
+++ data.py
@@ -1,26 +1,29 @@
 #use this list of menu items
-menu_items = [
-'D1 ​​SODA 3',
-'D2 ​​LEMONADE 3',
-'D3 ​​TEA 2',
-'D4 WATER 0',
-'A1 ​​POTATO_CAKES 7',
-'A2 ​​SPINACH_DIP 5',
-'A3 ​​OYSTERS 13',
-'A4 ​​CHEESE_FRIES 4',
-'A5 ​​ONION_RINGS 7',
-'S1 ​​COBB 14',
-'S2 ​​CAESAR 13',
-'S3 ​​GREEK 12',
-'E1 ​​BURGER 16',
-'E2 ​​PASTA 15',
-'E3 ​​GNOCCHI 14',
-'E4 ​​GRILLED_STEAK_SANDWICH 17',
-'T1 ​​CARAMEL_CHEESECAKE 13',
-'T2 ​​APPLE_COBBLER 12',
-'T3 ​​BROWNIE_SUNDAE 9',
-'T4 ​​FLAN 8'
-]
+import random
+menu_items = {
+'D1': ['SODA', 3],
+'D2': ['LEMONADE', 3],
+'D3': ['TEA' ,2],
+'D4': ['WATER', 0],
+'A1': ['POTATO_CAKES', 7, random.randint(25, 50)],
+'A2': ['SPINACH_DIP', 5, random.randint(25, 50)],
+'A3': ['OYSTERS', 13, random.randint(25, 50)],
+'A4': ['CHEESE_FRIES', 4, random.randint(25, 50)],
+'A5': ['ONION_RINGS', 7, random.randint(25, 50)],
+'S1': ['COBB', 14, random.randint(25, 50)],
+'S2': ['CAESAR', 13, random.randint(25, 50)],
+'S3': ['GREEK', 12, random.randint(25, 50)],
+'E1': ['BURGER', 16, random.randint(25, 50)],
+'E2': ['PASTA', 15, random.randint(25, 50)],
+'E3': ['GNOCCHI',14, random.randint(25, 50)],
+'E4': ['GRILLED_STEAK_SANDWICH',17, random.randint(25, 50)],
+'T1': ['CARAMEL_CHEESECAKE',13, random.randint(25, 50)],
+'T2': ['APPLE_COBBLER',12, random.randint(25, 50)],
+'T3': ['BROWNIE_SUNDAE',9, random.randint(25, 50)],
+'T4': ['FLAN', 8, random.randint(25, 50)]
+}
+
+
 drink_items = ['D1', 'D2',  'D3', 'D4']
 appetizer_items = ['A1', 'A2',  'A3', 'A4', 'A5']
 salad_items = ['S1', 'S2', 'S3']
